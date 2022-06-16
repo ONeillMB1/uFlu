cfc853c86107cfc7b9835ef8246c1fb803882afb
Mary <mary@lbc-b229-g1.lbc.espci.fr>
2022-06-16 15:42:14 +0200

    update shell scripts

--- scripts/02_run_PROCESS_part2.sh
+++ scripts/02_run_PROCESS_part2.sh
@@ -4,9 +4,11 @@
 #SBATCH -J uFlu
 #SBATCH -o logfile_customAnalyses_%A_%a.txt
 #SBATCH -e logfile_customAnalyses_%A_%a.txt
-#SBATCH --array=1-<UPDATE TO NUMBER OF SAMPLES> 
+#SBATCH --array=1-12
+#SBATCH --qos=fast
+#SBATCH --partition=dedicated
 
-PROJECT="<CHANGE_ME>"
+PROJECT="NGS11"
 TASKDIR="/pasteur/zeus/projets/p01/uFlu/reassortment_project"
 SAMPLE_NUM=${SLURM_ARRAY_TASK_ID}
 line=`head -n ${SAMPLE_NUM} ${TASKDIR}/02_STARsolo_outputs/${PROJECT}/${PROJECT}_samples.txt | tail -n 1`
@@ -21,8 +23,6 @@ hostname
 ${ID}
 ${SAMPLE_NUM}
 
-mkdir ${TASKDIR}/03_custom_analyses/${PROJECT}/${ID}
-
-Rscript ${TASKDIR}/resources/uFlu/scripts/process.R ${ID} ${PROJECT}
+Rscript ${TASKDIR}/resources/uFlu/scripts/process_part2_supplement.R ${ID} ${PROJECT}
 
 chmod -R 775 ${TASKDIR}/03_custom_analyses/${PROJECT}/${ID}
